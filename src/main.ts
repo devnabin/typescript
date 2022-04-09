96c0c126512f1657121cafb8bf929a1bcac22e05
Nabiin <nabin123456j@gmail.com>
2022-04-09 21:33:06 +0545

    - Project setup - Learned basic data types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,61 @@
-import './style.css'
+function nabin(a:number, b:number,isNumber:boolean,phraseTest:string){
+ if(isNumber){
+      return phraseTest + (a+b);
+ }else{
+   return 'no word'
+ }
+}
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const a = 13;
+let v = 5;
+let b:number = 0;
+b=2;
+v='hero';
+console.log(v);
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const isNumber:boolean = false;
+let phraseTest:string;
+phraseTest = 'The result is'
+
+const result = nabin(a,b,isNumber,phraseTest)
+console.log(result)
+
+
+
+
+
+const bb = 'hi';
+const bc = 23;
+
+let abc = 'hero';
+// abc = 23;
+
+let xyz:string;
+xyz = 'her';
+xyz = 'he'
+console.log(xyz);
+
+let test;
+test = 'heero'
+test = '12'
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './style.css'
+
+// const app = document.querySelector<HTMLDivElement>('#app')!
+
+// app.innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
