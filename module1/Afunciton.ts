cffc556366a3349806b46f21c6580497231429d2
nabiin <nabin123456j@gmail.com>
2023-04-17 18:20:20 +0545

    all about function

---
+++ module1/Afunciton.ts
@@ -0,0 +1,14 @@
+// function returing only string
+function a(): string {
+    return "Hello World";
+}
+
+// function returing only number
+function b(): number {
+    return 1;
+}
+
+//function should have no return type
+function c(): void {
+    console.log("if we type any return type then it will throw an error");
+}
