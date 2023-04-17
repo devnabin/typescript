edefe0644769adc7c3130bfbf7712ddaf40fe463
Nabin Bhandari <46418794+Nabiin@users.noreply.github.com>
2023-04-17 14:52:52 +0000

    function learning done

--- module1/Afunciton.ts
+++ module1/Afunciton.ts
@@ -12,3 +12,10 @@ function b(): number {
 function c(): void {
     console.log("if we type any return type then it will throw an error");
 }
+
+// function taking arguments
+function myFunc(a: number, b: number): number {
+    return a + b;
+}
+const myFuncValue = myFunc(11, 9);
+console.log(myFuncValue);
